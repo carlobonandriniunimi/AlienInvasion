6d3cc1876176d31c12b3351f71d4ecb015d206ec
carlobonandriniunimi <carlo.bonandrini@studenti.unimi.it>
2023-03-05 15:35:32 +0100

    set speed and screen boundaries for the ship

--- src/ship.py
+++ src/ship.py
@@ -3,9 +3,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_setting, screen):
         """Initialize the ship and set its starting positions."""
         self.screen = screen
+        self.ai_settings = ai_setting
 
         # * Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')  # Surface object
@@ -19,16 +20,23 @@ class Ship():
         # bottom of the ship is aligned with the bottom of the screen
         self.rect.bottom = self.screen_rect.bottom
 
+        # * Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+
         # * Movement flag
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on the movement flag."""
-        if self.moving_right:
-            self.rect.centerx += 1
-        elif self.moving_left:
-            self.rect.centerx -= 1
+        # * Updates the ship's center value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        elif self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Updates the rect object from self.center (only stores integer portion)
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the ship at its current location."""
